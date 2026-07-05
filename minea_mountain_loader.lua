537b50763837470952d8f5da6da5e388e035ecfb
TrueScent <aday44688@gmail.com>
2026-07-05 17:42:51 +0100

    v6 loader - cache bust update

--- minea_mountain_loader.lua
+++ minea_mountain_loader.lua
@@ -1,5 +1,6 @@
 -- Mine a Mountain v6 ULTIMATE Loader
 -- Execute this in your executor to load the full script
 -- Compatible with: Delta, Synapse X, KRNL, Fluxus, Wave, Codex
+-- Updated: 2026-07-05 16:42:51
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/TrueSc3nt/utils/main/minea_mountain_v6.lua", true))()
