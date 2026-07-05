a3dd973aa851a6fff791ea6f888e705245ca0cb6
TrueScent <aday44688@gmail.com>
2026-07-05 18:21:09 +0100

    Update loader v6

--- minea_mountain_loader.lua
+++ minea_mountain_loader.lua
@@ -1,5 +1,3 @@
--- Mine a Mountain v6 ULTIMATE Loader
--- Execute in your executor to load the full script
--- Compatible with Delta, Synapse X, KRNL, Fluxus, Wave, Codex
-
+--[[ Mine a Mountain v6 ULTIMATE Loader ]]
+-- Compatible with: Delta, Synapse X, KRNL, Fluxus, Wave, Codex
 loadstring(game:HttpGet("https://raw.githubusercontent.com/TrueSc3nt/utils/main/minea_mountain_v6.lua", true))()
