0c009f48852b9ffb8188178c3da88bea44e4e520
TrueScent <aday44688@gmail.com>
2026-07-05 17:44:06 +0100

    Delete old loader to bust CDN cache

--- minea_mountain_loader.lua
+++
@@ -1,7 +0,0 @@
--- Mine a Mountain v6 ULTIMATE Loader
--- Execute this in your executor to load the full script
--- Compatible with: Delta, Synapse X, KRNL, Fluxus, Wave, Codex
--- v6: Auto Farm, ESP, Exploits, Dupe, Anti-Detection
--- Last updated: 2026-07-05 16:43:12 UTC
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TrueSc3nt/utils/main/minea_mountain_v6.lua", true))()
