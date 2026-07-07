1208a49993f1ff538742e574f6e88b05d3fdbe5c
TrueSc3nt <TrueSc3nt@users.noreply.github.com>
2026-07-07 11:57:35 +0100

    Bump loader version to 17 for cache bust

--- minea_loader.lua
+++ minea_loader.lua
@@ -1,5 +1,5 @@
 --[[ Minea Hub v7 Loader | TrueSc3nt ]]
-local VERSION = "16"
+local VERSION = "17"
 local URL = "https://raw.githubusercontent.com/TrueSc3nt/utils/main/minea_clean_test.lua?v=" .. VERSION .. "&t=" .. tick()
 
 local StarterGui = game:GetService("StarterGui")
